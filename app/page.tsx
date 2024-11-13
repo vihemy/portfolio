ff0c606062848c637201f1b09848d226ec75b14b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-11-13 10:03:20 +0100

    change profie pic + changed link to linkedin

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-      <a href={socialLinks.twitter} target="_blank">
+      <a href={socialLinks.linkedin} target="_blank">
         <Image
-          src="/profile.png"
+          src="/kc_profil.jpg"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
