47cd5df3910b624aa5b312ac693c9abfe42c5540
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-11-13 10:38:42 +0100

    Add selected projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
+    title: "Idea Machine",
+    year: 2024,
+    description:
+      "A fantastical sci-fi machine that transforms user ideas on how to improve the ocean environment into imaginative illustrations, through a series of playful dashboards and DMX light fixture animations. Part of exhibition Dødvande (Dead Waters) at Danish aquarium Kattegatcentret.",
+    url: "https://github.com/vihemy/dinide",
+  },
+  {
+    title: "BrightSign Log Tools",
     year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "Python tools to download and process player logs from BrightSign mediaplayers configured on a local network using Local Diagnostic Web Server.",
+    url: "https://github.com/vihemy/brightsign-log-tools",
   },
   {
-    title: "Project Two",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
+    title: "Mød Mig I Mørket",
     year: 2021,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "This interactive audiowalk guides users through a choose-your-own-adventure drama set in Aarhus' Botanical Garden. The story unfolds uniquely, shaped by the pathways the user decides to explore in the park.",
+    url: "https://explore.echoes.xyz/collections/tuouST7zBLRMtaXS",
   },
 ];
