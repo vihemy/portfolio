e58783da747c8f96a3470068fedeb72ffa0dcb32
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-11-13 10:42:44 +0100

    edit order of navitems

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,8 +3,8 @@ import { ThemeSwitch } from "./theme-switch";
 import { metaData } from "../config";
 
 const navItems = {
-  "/blog": { name: "Blog" },
   "/projects": { name: "Projects" },
+  "/blog": { name: "Blog" },
   // "/photos": { name: "Photos" },
 };
 
