a24555bfcfab4abccd04df0c0a9f365dad6c562e
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-11-13 10:23:48 +0100

    first draft of intro text

--- app/page.tsx
+++ app/page.tsx
@@ -17,50 +17,32 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio, made simple!
+        Victor Hempel-Mydtskov
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS for optimal performance.
+          As a developer and interaction designer I work in the space between
+          logical systems and fallible humans.
         </p>
         <p>
-          Nextfolio includes all the essentials for a stunning portfolio: SEO,
-          MDX support, RSS, Atom, & JSON feeds, analytics, tweet & YouTube
-          embeds, KaTeX integration, and{" "}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
+          I am passionate about creating digital experiences that are both
+          intuitive and delightful.
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
+          This is my portfolio/blog where I share projects and
+          thoughts about creating and learning.
+        </p>
+        <p>
+          Check out my{" "}
+          <a target="_blank" href={socialLinks.linkedin}>
+            LinkedIn
           </a>{" "}
-          and fully customizable, making it easy to add more features.
-        </p>
-        <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
+          for my professional and educational background or visit my{" "}
+          <a target="_blank" href={socialLinks.github}>
+            GitHub
           </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
+          to browse my code.
         </p>
       </div>
     </section>
